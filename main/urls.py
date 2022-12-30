e63d3ac64ca98cc8226ee7a2673881cf97174174
dishantvyas15 <keenlearner13579@gmail.com>
2022-12-30 09:00:39 +0530

    beautify front-end

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from main import views
 urlpatterns = [
     path('', views.root, name="root"),
     path('index', views.index, name='index'),
+    path('input', views.input, name='input'),
     path('analyze_reqs', views.analyze_reqs, name='analyze_reqs')
 ]
